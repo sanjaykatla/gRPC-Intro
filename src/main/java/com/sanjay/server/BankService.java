f606454a7708d96af545803a7be2ec99469b693c
skatla <sanjaykatla.sanjay@gmail.com>
2023-04-03 23:16:57 +0700

    add error codes and postman snaps

--- src/main/java/com/sanjay/server/BankService.java
+++ src/main/java/com/sanjay/server/BankService.java
@@ -2,6 +2,7 @@ package com.sanjay.server;
 
 
 import com.sanjay.models.*;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -23,15 +24,18 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
         int balance = AccountsDatabase.getBalance(accountNumber);
 
+        if(balance < amount){
+            Status status = Status.FAILED_PRECONDITION.withDescription("No enough money." +
+                    "You have only " + balance);
+            responseObserver.onError(status.asRuntimeException());
+            return;
+        }
+
+        // all validation passed
         for(int i = 0; i< (amount / 10); i++){
             Money money = Money.newBuilder().setValue(10).build();
             responseObserver.onNext(money);
             AccountsDatabase.deductBalance(accountNumber, 10);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
         }
         responseObserver.onCompleted();
     }
