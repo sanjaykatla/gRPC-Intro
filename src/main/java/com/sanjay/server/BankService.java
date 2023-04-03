3ce72f5a26027c4a170f31a1e60693fbd7068031
skatla <sanjaykatla.sanjay@gmail.com>
2023-04-03 22:51:27 +0700

    server streaming service

--- src/main/java/com/sanjay/server/BankService.java
+++ src/main/java/com/sanjay/server/BankService.java
@@ -1,9 +1,7 @@
 package com.sanjay.server;
 
 
-import com.sanjay.models.Balance;
-import com.sanjay.models.BalanceCheckRequest;
-import com.sanjay.models.BankServiceGrpc;
+import com.sanjay.models.*;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -15,6 +13,26 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                 .setAmount(AccountsDatabase.getBalance(accountNumber))
                 .build();
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+        int accountNumber = request.getAccountNumber();
+        int amount = request.getAmount();
+
+        int balance = AccountsDatabase.getBalance(accountNumber);
+
+        for(int i = 0; i< (amount / 10); i++){
+            Money money = Money.newBuilder().setValue(10).build();
+            responseObserver.onNext(money);
+            AccountsDatabase.deductBalance(accountNumber, 10);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         responseObserver.onCompleted();
     }
 }
